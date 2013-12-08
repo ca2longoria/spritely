c235940e42601724dd12d3596fa63475e3e34afb
ca2longoria <ca2longoria@gmail.com>
2013-12-07 21:41:02 -0600

    Rearranged the main Spritely class
    Spritely.java is now located in the net.synapsehaven.sprite package.
    The Plugins class is now also its own separate file.

--- src/net/synapsehaven/spritely/Spritely.java
+++ src/net/synapsehaven/spritely/Spritely.java
@@ -1,14 +1,10 @@
+package net.synapsehaven.spritely;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.Map;
 
-import fi.iki.elonen.NanoHTTPD;
-import fi.iki.elonen.NanoHTTPD.Response;
 import fi.iki.elonen.ServerRunner;
 import fi.iki.elonen.SimpleWebServer;
-import fi.iki.elonen.WebServerPlugin;
+
 public class Spritely
 {
 	public static void main(String[] args)
@@ -25,42 +21,4 @@ public class Spritely
 		ServerRunner.executeInstance(serv);
 	}
 	
-	
-	private static class Plugins
-	{
-		private static interface LocalWebServerPlugin extends WebServerPlugin
-		{
-			public void registerWithSimpleWebServer();
-		}
-		
-		private static class ImageSvgXmlPlugin implements LocalWebServerPlugin
-		{
-			@Override public void registerWithSimpleWebServer()
-			{
-				SimpleWebServer.registerPluginForMimeType(
-					new String[]{"null.svg"}, // these are necessary to add extensiosn to MIME_TYPES
-					"image/svg+xml", // mime-type
-					this, // WebServerPlugin
-					null // command line options
-				);
-			}
-			
-			@Override public boolean canServeUri(String uri, File rootDir)
-			{ return new File(rootDir,uri).exists(); }
-	
-			@Override public void initialize(Map<String, String> commandLineOptions) {}
-			
-			@Override public Response serveFile(String uri, Map<String, String> headers,
-				File file, String mimeType)
-			{
-				FileInputStream fin = null;
-				try { fin = new FileInputStream(file); }
-				catch (FileNotFoundException e)
-				{ e.printStackTrace(); }
-				
-				return new NanoHTTPD.Response(
-					Response.Status.OK, "image/svg+xml", fin);
-			}	
-		}
-	}
 }
